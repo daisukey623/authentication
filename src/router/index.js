367487deaeeb0253d9f4092de9a4cfcb1e13dd35
daisukey623 <42987402+daisukey623@users.noreply.github.com>
2021-05-16 12:08:12 +0900

    vue課題4-2

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,21 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-
+import Comments from '../views/Comments.vue';
+import Login from '../views/Login.vue';
 import Register from '../views/Register.vue';
 
 
 Vue.use(VueRouter)
 
 const routes = [
+  {
+    path: '/',
+    component: Comments
+  },
+  {
+    path: '/login',
+    component: Login
+  },
   {
     path: '/register',
     component: Register
