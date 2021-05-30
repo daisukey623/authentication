de3fd8dadc296eab3c37a7c2d2ba4f52fa7b6d4d
daisukey623 <42987402+daisukey623@users.noreply.github.com>
2021-05-30 12:48:22 +0900

    Add log out
    ・ダッシュボードにログアウトボタン配置
     ・ログアウトボタン押下で、ログアウトされログイン画面へ遷移
    ・ログアウトされた状態でダッシュボードへアクセスすると、ログイン画面へリダイレクト

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,6 +2,7 @@
   <div>
     <div>{{ userName }}さんようこそ</div>
     <div>残高：{{ coin }}</div>
+    <div><button @click="logOut()">ログアウト</button></div>
     <h2>ユーザー一覧</h2>
     <router-link to="/register">新規登録はこちらから</router-link>
   </div>
@@ -9,21 +10,30 @@
 
 <script>
 import firebase from 'firebase/app';
+import { auth } from '../main';
 export default {
   created() {
     firebase.auth().onAuthStateChanged((user) => {
-      if (user) {
+      if (!user) {
+        this.$router.push('/login');
+        window.alert('ログアウトしました');
+        } else {
         this.$store.dispatch('getUser');
       }
     });
   },
-  computed:{
-    userName(){ 
-      return this.$store.getters.userName
+  computed: {
+    userName() {
+      return this.$store.getters.userName;
     },
-    coin(){
-      return this.$store.getters.coin
-    }
-  }
+    coin() {
+      return this.$store.getters.coin;
+    },
+  },
+  methods: {
+    logOut: async function() {
+     await auth.signOut();
+    },
+  },
 };
 </script>
